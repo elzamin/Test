9237c96e397fce933fe0dca0f81a49eaa683d5f9
elzamin <elzamin006@mail.ru>
2016-12-23 04:35:36 +0400

    Add files via upload

---
+++ headers/systemmonitor.h
@@ -0,0 +1,16 @@
+#ifndef SYSTEMMONITOR_H
+#define SYSTEMMONITOR_H
+
+#include <QtWidgets>
+#include <QtGui>
+#include "info.h"
+#include "processtable.h"
+
+class SystemMonitor : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit SystemMonitor(QWidget *parent = 0);
+};
+
+#endif // SYSTEMMONITOR_H
